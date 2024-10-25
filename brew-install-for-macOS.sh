debe653f4da58bfb947ad2acd308734c61a9eb5f
Toshihiro Takehara aka nouphet <nouphet@craftsman-software.com>
2024-10-25 11:39:23 +0900

    Update to latest.

--- brew-install-for-macOS.sh
+++ brew-install-for-macOS.sh
@@ -1,5 +1,26 @@
-# For business needs
 brew update
+
+# For Development command line tools
+brew install ack
+brew install awscli
+brew install starship
+brew install jq
+brew install wget
+brew install go
+brew install python
+brew install rust
+brew install docker-compose
+brew install minikube
+brew install kubernetes-cli
+brew install kind
+brew install k6
+brew install k9s
+brew install tfenv
+brew install kubectx
+brew install tig
+
+
+# For business needs
 brew install --cask alfred
 brew install --cask iterm2
 brew install --cask google-chrome
@@ -8,15 +29,25 @@ brew install --cask firefox-developer-edition
 brew install --cask google-japanese-IME
 brew install --cask 1password
 brew install --cask dropbox
+brew install --cask evernote
+brew install --cask finicky
 brew install --cask slack zoom microsoft-teams
+brew install --cask docker
 brew install --cask visual-studio-code
 brew install --cask atom
 brew install --cask jetbrains-toolbox
-brew install tig
 brew install --cask toggl-track
 brew install --cask discord
 brew install --cask vnc-viewer
 brew install --cask microsoft-edge
+brew install --cask gitkraken gitkraken-cli
+brew install --cask lens
+brew install --cask macdown
+brew install --cask tunnelblick
+brew install --cask github/copilot-cli/copilot github/copilot-cli
+
+brew tap lencx/chatgpt https://github.com/lencx/ChatGPT.git
+brew install --cask chatgpt --no-quarantine
 
 # Personal needs
 brew install --cask chatwork
